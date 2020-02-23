cbf0faf548dfc35d799898178bf7e8c3461e5776
IKE LEE <ikelee17@gmail.com>
2020-02-24 00:37:39 +0900

    new commit

---
+++ numpy/pythoncode.py
@@ -0,0 +1,7 @@
+# this is a python file created in jupyter notbook
+def list_fruits(fruits=[]):
+    for fruit in fruits:
+        print(fruit)
+        
+fruits = ['apple','kiwi','banana']
+list_fruit(fruits)
